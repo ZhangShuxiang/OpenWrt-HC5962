31f8e10fdce455906f96517e1fd0324ad9054ac1
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2024-03-18 15:06:29 +0800

    Update diy-part1.sh

--- neo2/diy-part1.sh
+++ neo2/diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 # echo src-git-full openwrt-vlmcsd https://github.com/cokebar/openwrt-vlmcsd.git^213bce995e5ce6af78fb923db1838c992b21950c >>feeds.conf.default
 # echo src-git-full luci-app-vlmcsd https://github.com/cokebar/luci-app-vlmcsd.git^7f9cd13ccc7d9b5c6483501f43973cd4a1a7c309 >>feeds.conf.default
-echo src-git packages/firewall4 https://github.com/openwrt/firewall4.git>>feeds.conf.default
+echo src-git firewall4 https://github.com/openwrt/firewall4.git>>feeds.conf.default
